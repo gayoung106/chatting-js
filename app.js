c10fc36c93a8e3b37738c79ebd4600cfec6a291f
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-09-13 23:22:02 +0900

    complete

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const app = express();
 const path = require("path");
 const server = http.createServer(app);
 //server안에 app을 담아서 궁극적으로 express가 http를 통해서 실행될 수 있도록
-
 const socketIO = require("socket.io");
+const moment = require("moment");
+
 const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "src")));
 const PORT = process.env.PORT || 5000;
-
+//서버에서는 보내주는 내용까지만 구현
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
-    console.log(data);
-    io.emit("chatting", data);
+    const { name, msg } = data;
+    io.emit("chatting", {
+      name,
+      msg,
+      time: moment(new Date()).format("h:ss A"),
+    });
   });
 });
 
